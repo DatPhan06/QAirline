81871507c3eb51e0139da7bba2f67c8c43b8fdad
Phan Tiến Đạt <126739355+DatPhan06@users.noreply.github.com>
2024-12-05 03:12:20 +0700

    Add show seats in flights info

--- frontend/src/components/FlightDetail.js
+++ frontend/src/components/FlightDetail.js
@@ -1,10 +1,12 @@
 // FlightDetail.js
 import React, { useEffect, useState } from "react";
 import styles from "./FlightDetail.module.css";
-import { getAirplaneById } from "../services/airplaneService"; // Import hàm lấy thông tin máy bay
+import { getAirplaneById } from "../services/airplaneService";
+import { getSeatsByAirplaneId } from "../services/seatService";
 
 const FlightDetail = ({ flight }) => {
   const [airplane, setAirplane] = useState(null);
+  const [seats, setSeats] = useState([]);
 
   useEffect(() => {
     const fetchAirplaneDetails = async () => {
@@ -16,7 +18,17 @@ const FlightDetail = ({ flight }) => {
       }
     };
 
+    const fetchSeats = async () => {
+      try {
+        const data = await getSeatsByAirplaneId(flight.airplane_id);
+        setSeats(data);
+      } catch (error) {
+        console.error("Error fetching seats:", error);
+      }
+    };
+
     fetchAirplaneDetails();
+    fetchSeats();
   }, [flight.airplane_id]);
 
   if (!airplane) {
@@ -43,15 +55,15 @@ const FlightDetail = ({ flight }) => {
           <p>Sức chứa: {airplane.seat_capacity}</p>
 
           {/* Hiển thị danh sách ghế */}
-          {/* <h2>Danh sách chỗ ngồi</h2>
+          <h2>Danh sách chỗ ngồi</h2>
           <ul className={styles.seatList}>
-            {airplane.seats.map((seat) => (
+            {seats.map((seat) => (
               <li key={seat.seat_id} className={styles.seatItem}>
                 Ghế số: {seat.seat_number}, Loại ghế: {seat.seat_class}, Trạng
                 thái: {seat.status}
               </li>
             ))}
-          </ul> */}
+          </ul>
         </>
       )}
 
