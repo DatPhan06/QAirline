977523f8d87d78705de0c49cb846a65f9d4cd512
chouchoussyuet <chouchoussy@gmail.com>
2024-12-07 01:55:18 +0700

    homepage ver2

---
+++ frontend/src/components/SlideShow.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import styles from './SlideShow.module.css'; // Tạo file CSS Module riêng cho Slideshow
+
+const SlideShow = ({ images, interval = 3000 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, interval);
+
+    return () => clearInterval(slideInterval);
+  }, [images.length, interval]);
+
+  return (
+    <div className={styles.slideshowContainer}>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Slideshow ${index + 1}`}
+          className={`${styles.slide} ${
+            index === currentIndex ? styles.active : ''
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SlideShow;
