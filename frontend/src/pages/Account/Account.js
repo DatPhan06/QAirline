0d491e8d992654813aac6e33726e2504cd5495c1
Phan Tiến Đạt <126739355+DatPhan06@users.noreply.github.com>
2024-12-21 18:01:00 +0700

    new structure

--- frontend/src/pages/Account/Account.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Account = () => {
-  return <h1>Trang Tài Khoản</h1>;
-};
-
-export default Account;
--- frontend/src/pages/Explore/Explore.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Explore = () => {
-  return <h1>Trang Khám Phá</h1>;
-};
-
-export default Explore;
